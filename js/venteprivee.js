f1b09b56654f9153b02a821a798db5df2e1cae99
erwincalvez <erwincalvez@gmail.com>
2019-11-29 11:28:13 +0100

    skip links et focus

--- js/venteprivee.js
+++ js/venteprivee.js
@@ -8,9 +8,21 @@ $( document ).ready(function() {
 
 
   const el = document.querySelector('.nav-menu');
-  el.inert = true; // alternatively, el.setAttribute('inert', '');
+    el.inert = true;
+   // alternatively, el.setAttribute('inert', '');
 
   $('.toggle-menu').click(function(){
+    $('.nav-menu').toggleClass('nav-menu-is-showing');
+    if($('.nav-menu').hasClass('nav-menu-is-showing')) {
+      el.inert = false;
+    }
+    else {
+      el.inert = true;
+    }
+
+  });
+
+  $('.skip-link--toggle-menu').click(function(){
     $('.nav-menu').toggleClass('nav-menu-is-showing');
     if($('.nav-menu').hasClass('nav-menu-is-showing')) {
       el.inert = false;
